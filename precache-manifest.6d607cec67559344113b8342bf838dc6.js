9861f7916f9716d059d2ac0939b6e11435489c49
Marshall Ku <marshall@kakao.com>
2020-07-08 17:03:25 +0900

    Updates

---
+++ precache-manifest.6d607cec67559344113b8342bf838dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08042c00f862cce7253e13d73134eea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e1a81fabeb0cc83236",
+    "url": "/static/css/main.02bcbcad.chunk.css"
+  },
+  {
+    "revision": "40a35d17f239c9c40ef7",
+    "url": "/static/js/2.6a0fc1b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6a0fc1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e1a81fabeb0cc83236",
+    "url": "/static/js/main.44ef244e.chunk.js"
+  },
+  {
+    "revision": "4f307d88eed4c60b8a00",
+    "url": "/static/js/runtime-main.c38a86bd.js"
+  }
+]);
